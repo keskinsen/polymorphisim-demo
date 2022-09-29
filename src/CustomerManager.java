a0448846b2b5027f674564953f2284236600dc50
unknown <pelin.keskinn@hotmail.com>
2022-09-29 19:18:50 +0300

    first commit

---
+++ src/CustomerManager.java
@@ -0,0 +1,10 @@
+public class CustomerManager {
+    private BaseLogger logger;
+    public CustomerManager(BaseLogger logger){
+        this.logger=logger;
+    }
+    public void add(){
+        System.out.println("musteri eklendi");
+        this.logger.log("log mesaji");
+    }
+}
